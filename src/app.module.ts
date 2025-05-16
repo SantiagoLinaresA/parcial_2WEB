137b1209125f6cf839548fac098aec455a7b9fc9
SantiagoLinaresA <s.linaresa@uniandes.edu.co>
2025-05-16 08:52:54 -0500

    creacion proyecto

---
+++ src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EstudianteModule } from './estudiante/estudiante.module';
+import { ActividadModule } from './actividad/actividad.module';
+import { ResenaModule } from './resena/resena.module';
+
+@Module({
+  imports: [EstudianteModule, ActividadModule, ResenaModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
